7b93846bc4835283e1f3dbfd697e7d5a4c6c69f3
James McCarthy <jmccarth@uwaterloo.ca>
2015-03-20 14:16:07 -0400

    Updates

--- goose_updater.sh
+++ goose_updater.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 cd /home/jmccarth/goosewatch-auto
 python goose_auto.py
 cd /home/jmccarth/Datasets
+git pull
 statusout=$(git status -s)
 if [ "$statusout" = "" ]; then
   echo No changes
 else
   echo Changes
+  git add GooseWatch/1151GooseWatch.csv
+  git commit -m "Automated update to goosewatch data"
 fi
